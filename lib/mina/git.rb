f21b916496ec725b6accd5c9602c0d724bde5bc0
Aditya Sanghi <aditya.sanghi@risingsuntech.net>
2014-04-21 20:17:13 +0530

    i like to have the .git available

--- lib/mina/git.rb
+++ lib/mina/git.rb
@@ -53,7 +53,6 @@ namespace :git do
       echo "-----> Using this git commit" &&
       echo &&
       #{echo_cmd %[git --no-pager log --format="%aN (%h):%n> %s" -n 1]} &&
-      #{echo_cmd %[rm -rf .git]} &&
       echo
     ]
 
